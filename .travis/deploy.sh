2e1b1839ccae108195c1bc2aeb7905969f4fc44c
Russell Hay <me@russellhay.com>
2016-01-24 01:00:40 +0000

    Implement Travis CI deployment

---
+++ .travis/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Do not deploy if it's a pull request
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo Skipping deploy because pull request
+  exit 0
+fi
+
+case "$TRAVIS_BRANCH" in
+  master)
+    ;;
+  travis)
+    ;;
+  *)
+    echo Skipping deploy because not a selected branch
+    exit 0
+
+esac
+
+cd build
+git init
+git config user.name "Travis CI"
+git config user.email "noone@nowhere.com"
+
+git add .
+git commit -m "auto-commit via travis"
+
+git push --force --quiet "https://${GH_DEPLOYER}:${GH_TOKEN}@${GH_REPO}" master:gh-pages > /dev/null 2>&1
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SuperSpy website source
 
+[![Build Status](https://travis-ci.org/poorimpulse/fate-superspy.svg?branch=master)](https://travis-ci.org/poorimpulse/fate-superspy)
+
 To view go to [poorimpulse.github.io/fate-superspy](https://poorimpulse.github.io/fate-superspy)
 
 ## To build
@@ -9,8 +11,6 @@ wintersmith build
 ```
 
 ## To Deploy (you need commit privs)
-
-*coming soon*
 
 ```
 gulp deploy
